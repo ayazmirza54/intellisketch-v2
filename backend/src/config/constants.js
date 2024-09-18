85066948c475ec031be9d4f04f441d4e82f70049
Ayaz Mirza <37616943+ayazmirza54@users.noreply.github.com>
2024-09-18 23:30:58 +0530

    Update constants.js

--- backend/src/config/constants.js
+++ backend/src/config/constants.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     SERVER_URL: '0.0.0.0',
-    PORT: process.env.PORT || 8900,
+    PORT: process.env.PORT || 80,
     ENV: process.env.NODE_ENV || 'development',
     GEMINI_API_KEY: process.env.GEMINI_API_KEY
 };
